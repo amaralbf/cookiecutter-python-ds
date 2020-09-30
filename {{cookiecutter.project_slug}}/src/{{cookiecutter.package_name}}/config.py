7936760e8b919eff555eb7df1241ce698b2e51a0
Bruno Amaral <amaralbf@gmail.com>
2020-09-29 22:53:17 -0300

    Set project_dir and log files in config.py

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/config.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/config.py
@@ -1,5 +1,14 @@
+import inspect
+from pathlib import Path
+
 from loguru import logger
 
+import {{cookiecutter.package_name}}
 
+PROJECT_DIR = Path(inspect.getfile({{cookiecutter.package_name}})).resolve().parents[2]
+
+{% raw %}
 def configure_logger():
-    logger.add("{{cookiecutter.project_slug}}_{level}_{time}.log")
+    logger.add(f"{PROJECT_DIR}/logs/{{time}}_debug_my_ds_project.log", level='DEBUG')
+    logger.add(f"{PROJECT_DIR}/logs/{{time}}_info_my_ds_project.log", level='INFO')
+{% endraw %}
